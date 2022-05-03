f4f33621dd660bfb41fc0d814ae9fca394e58efe
Gui <guilherme.rddev@gmail.com>
2022-05-03 15:22:24 -0300

    feat: add funcao para botao voltar

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -4,15 +4,24 @@ import { CloseButton } from "../../CloseButton";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
+    onFeedbackRestartRequested: () => void;
 };
 
-export function FeedbackContentStep({ feedbackType }: FeedbackContentStepProps) {
+export function FeedbackContentStep({ 
+    feedbackType, 
+    onFeedbackRestartRequested 
+}: FeedbackContentStepProps) {
     const feedbackTypesInfo = feedbackTypes[feedbackType];
     return (
         <>
             <header>
-                <button type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100">
-                    <ArrowLeft weight="bold" className="w-4 h-4"/>
+                <button 
+                type="button" 
+                className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+                onClick={onFeedbackRestartRequested}
+                >
+                    
+                    <ArrowLeft weight="bold" className="w-4 h-4" />
                 </button>
                 <span className="text-xl leading-6 flex items-center gap-2">
                     <img src={feedbackTypesInfo.image.source} alt={feedbackTypesInfo.image.alt} className="w-6 h-6" />
