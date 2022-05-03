676f12bce7a10bec52ea15e83e97219386f5fb87
Gui <guilherme.rddev@gmail.com>
2022-05-03 15:15:35 -0300

    feat: add imagens aos conteudos

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -10,7 +10,8 @@ export function FeedbackContentStep({ feedbackType }: FeedbackContentStepProps) 
     return (
         <>
             <header>
-                <span className="text-xl leading-6">
+                <span className="text-xl leading-6 flex items-center gap-2">
+                    <img src={feedbackTypesInfo.image.source} alt={feedbackTypesInfo.image.alt} className="w-6 h-6" />
                     {feedbackTypesInfo.title}
                 </span>
                 <CloseButton />
