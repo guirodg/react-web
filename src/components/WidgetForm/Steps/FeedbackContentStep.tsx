c067507a3b84091b20a7e6ffdd9a09e44902f649
Gui <guilherme.rddev@gmail.com>
2022-05-03 15:48:29 -0300

    feature: add textarea e estilo

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -3,35 +3,42 @@ import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 
 interface FeedbackContentStepProps {
-    feedbackType: FeedbackType;
-    onFeedbackRestartRequested: () => void;
+	feedbackType: FeedbackType;
+	onFeedbackRestartRequested: () => void;
 };
 
-export function FeedbackContentStep({ 
-    feedbackType, 
-    onFeedbackRestartRequested 
+export function FeedbackContentStep({
+	feedbackType,
+	onFeedbackRestartRequested
 }: FeedbackContentStepProps) {
-    const feedbackTypesInfo = feedbackTypes[feedbackType];
-    return (
-        <>
-            <header>
-                <button 
-                type="button" 
-                className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
-                onClick={onFeedbackRestartRequested}
-                >
-                    
-                    <ArrowLeft weight="bold" className="w-4 h-4" />
-                </button>
-                <span className="text-xl leading-6 flex items-center gap-2">
-                    <img src={feedbackTypesInfo.image.source} alt={feedbackTypesInfo.image.alt} className="w-6 h-6" />
-                    {feedbackTypesInfo.title}
-                </span>
-                <CloseButton />
-            </header>
+	const feedbackTypesInfo = feedbackTypes[feedbackType];
+	return (
+		<>
+			<header>
+				<button
+					type="button"
+					className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+					onClick={onFeedbackRestartRequested}
+				>
 
-            <div className="flex py-8 gap-2 w-full">
-            </div>
-        </>
-    );
+					<ArrowLeft weight="bold" className="w-4 h-4" />
+				</button>
+				<span className="text-xl leading-6 flex items-center gap-2">
+					<img src={feedbackTypesInfo.image.source} alt={feedbackTypesInfo.image.alt} className="w-6 h-6" />
+					{feedbackTypesInfo.title}
+				</span>
+				<CloseButton />
+			</header>
+
+
+			<form className="my-4 w-full">
+				<textarea
+					className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thin scrollbar-thumb-zinc-700 scrollbar-track-transparent"
+					placeholder="Conte com detalhes o que está acontecendo..."
+
+				/>
+
+			</form>
+		</>
+	);
 }
