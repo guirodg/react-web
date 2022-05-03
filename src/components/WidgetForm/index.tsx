1fea06c0ad926f0e6fd0228035284d51050a51b9
Gui <guilherme.rddev@gmail.com>
2022-05-03 19:02:29 -0300

    feat: envio sucesso

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -40,13 +40,14 @@ export function WidgetForm() {
   const [feedbackSent, setFeedbackSent] = useState(false)
 
   function handleRestartFeedback() {
+    setFeedbackSent(false)
     setFeedbackType(null);
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       {feedbackSent ? (
-        <FeedbackSuccesstStep />
+        <FeedbackSuccesstStep onFeedbackRestartRequested={handleRestartFeedback}/>
       ) : (
         <>
           {!feedbackType ? (
