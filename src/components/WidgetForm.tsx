fbb08a0a9a40c4acc54d5d305b4eeec5fdfbcaa4
Gui <guilherme.rddev@gmail.com>
2022-05-03 11:06:22 -0300

    feat: add imagens para um objeto

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,5 +1,33 @@
 import { CloseButton } from "./CloseButton";
 
+import bugImageUrl from '../assets/bug.svg'
+import ideaImageUrl from '../assets/idea.svg'
+import thoughtImageUrl from '../assets/thought.svg'
+
+const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto'
+    }
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada'
+    }
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Imagem de um balão de pensamento'
+    }
+  },
+};
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -8,7 +36,9 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <p>Hello World</p>
+      <div className="flex py-8 gap-2 w-full">
+        <button></button>
+      </div>
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pelo <a className="underline underline-offset-2" href="https://www.linkedin.com/in/guird/">Guirodg</a>
