a3f9c07e4eba79b89b2d350946bc64ffffb38881
Gui <guilherme.rddev@gmail.com>
2022-05-07 08:44:41 -0300

    feat: cria arquivo separado para rotas

---
+++ server/src/routes.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
+
+export const routes = express.Router();
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "7358af154b7d24",
+    pass: "751e191a54ef88",
+  },
+});
+
+routes.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <cliente@gmail.com>",
+    to: "Guilherme Rodrigues <guilherme.rddev@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color:#111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
+  });
+
+  return res.status(201).json({ data: feedback });
+});
